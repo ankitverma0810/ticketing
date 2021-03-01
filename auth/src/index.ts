460c044799f88011e0a4a6d0036f2f733b520933
Ankit Verma <ankitverma_0810@yahoo.co.in>
2021-03-01 15:47:08 +0530

    Modified startup command

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 
-const start = async () => {	
+const start = async () => {
+	console.log('Starting up the auth service...');
+	
 	// validating whether secret key has been set in the deployment file or not.
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
